a107593f2b02a2032b18b69d119e3281428189ff
Will <wrjones104>
2022-02-02 15:10:15 -0500

    Added generate_seed functionality in functions/generate_seed.py

---
+++ functions/generate_seed.py
@@ -0,0 +1,30 @@
+import json
+import os
+import requests
+
+
+def generate_seed(flags, seed_desc):
+    url = "https://ff6wc.com/api/generate"
+    if seed_desc:
+        payload = json.dumps({
+            "key": os.getenv("ff6wc_api_key"),
+            "flags": flags,
+            "description": seed_desc
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+    else:
+        payload = json.dumps({
+            "key": os.getenv("ff6wc_api_key"),
+            "flags": flags
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    data = response.json()
+    if 'url' not in data:
+        print(f'API returned {data} for the following flagstring:\n{flags}')
+        return AttributeError
+    return data
