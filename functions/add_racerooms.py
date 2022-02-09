759b6dcdb6b11909cad5c3d9c45b0378bc7ab205
WhoDat42 <whodatfortytwo@gmail.com>
2022-02-08 23:42:08 -0600

    minor fix

--- functions/add_racerooms.py
+++ functions/add_racerooms.py
@@ -5,6 +5,7 @@ import random
 import string
 
 def add_racerooms(user, room_id, room_type, spoiler_room, ts):
+    pass
     f = open("db/races.txt", "a")
     writemsg = str([user, room_id, room_type, spoiler_room, ts])+"\n"
     f.write(writemsg)
